9f9eecc57db5af8262d5dc81286ca53496ad22bf
Matt Rothenberg <mattrothenberg@github.com>
2021-10-21 10:16:06 -0400

    feat: prettier, use GitHub style

--- components/file-viewer-with-toggle.tsx
+++ components/file-viewer-with-toggle.tsx
@@ -1,7 +1,8 @@
 import { DirectoryItem } from "hooks";
 import React, { useMemo, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { Grid } from '@githubocto/flat-ui';
+import github from "react-syntax-highlighter/dist/cjs/styles/hljs/github";
+import { Grid } from "@githubocto/flat-ui";
 
 interface FileViewerProps {
   data: DirectoryItem;
@@ -13,20 +14,25 @@ export function FileViewer(props: FileViewerProps) {
   const [viewerType, setViewerType] = useState("");
 
   const code = Buffer.from(content, "base64").toString();
-  const viewer = viewers.find(d => d.id === viewerType) || {} as any;
-  const Viewer = viewer.component || CodeViewer
+  const viewer = viewers.find((d) => d.id === viewerType) || ({} as any);
+  const Viewer = viewer.component || CodeViewer;
 
   return (
     <div className="border overflow-hidden rounded">
       <div className="border-b p-2 py-3 bg-gray-100 flex items-center justify-between">
         <div className="flex items-center">
           <span className="inline-block font-bold">{name}</span>
-          <span className="ml-2 text-sm font-mono text-gray-500">{size.toLocaleString()} bytes</span>
+          <span className="ml-2 text-sm font-mono text-gray-500">
+            {size.toLocaleString()} bytes
+          </span>
         </div>
         <div>
-          <select value={viewerType} onChange={e => setViewerType(e.target.value)}
-            className="form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border border-gray-300  rounded focus:outline-none focus:border-blue-500 sm:text-sm sm:leading-5">
-            {viewers.map(d => (
+          <select
+            value={viewerType}
+            onChange={(e) => setViewerType(e.target.value)}
+            className="form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border border-gray-300  rounded focus:outline-none focus:border-blue-500 sm:text-sm sm:leading-5"
+          >
+            {viewers.map((d) => (
               <option key={d.id} value={d.id}>
                 {d.label}
               </option>
@@ -53,7 +59,7 @@ class ErrorBoundary extends React.Component {
   }
 
   componentDidCatch(error, errorInfo) {
-    console.log(error)
+    console.log(error);
   }
 
   render() {
@@ -66,20 +72,23 @@ class ErrorBoundary extends React.Component {
   }
 }
 
-const viewers = [{
-  id: "code",
-  label: "Code",
-  component: CodeViewer
-}, {
-  id: "flat",
-  label: "Flat Data",
-  component: FlatViewer
-}]
+const viewers = [
+  {
+    id: "code",
+    label: "Code",
+    component: CodeViewer,
+  },
+  {
+    id: "flat",
+    label: "Flat Data",
+    component: FlatViewer,
+  },
+];
 
 function CodeViewer({ contents }: { contents: string }) {
   return (
     <div className="text-sm bg-white">
-      <SyntaxHighlighter>{contents}</SyntaxHighlighter>
+      <SyntaxHighlighter style={github}>{contents}</SyntaxHighlighter>
     </div>
   );
 }
@@ -88,12 +97,10 @@ function FlatViewer({ contents }: { contents: string }) {
     try {
       return JSON.parse(contents);
     } catch (e) {
-      return []
+      return [];
     }
   }, [contents]);
-  console.log(data)
+  console.log(data);
 
-  return (
-    <Grid data={data} />
-  );
+  return <Grid data={data} />;
 }

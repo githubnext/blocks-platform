cd68320ae0fb4ce0209c17e68aa5a34bbcf0df53
Amelia Wattenbeger <wattenberger@github.com>
2021-10-26 14:41:36 -0400

    feat: add releases to markdown viewer

--- components/viewers/markdown.tsx
+++ components/viewers/markdown.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
-import remarkMdx from 'remark-mdx'
 import MDX from '@mdx-js/runtime'
+import { timeFormat } from "d3"
 
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { ViewerProps } from ".";
@@ -8,15 +8,6 @@ import { octokit, useUpdateFileContents } from "hooks";
 import { Box, Button, StateLabel } from "@primer/components";
 import { ErrorBoundary } from "components/error-boundary";
 
-declare global {
-  namespace JSX {
-    // this merges with the existing intrinsic elements, adding 'my-custom-tag' and its props
-    interface IntrinsicElements {
-      'Issues': { 'children': string }
-      'myCustomTag': { 'children': string }
-    }
-  }
-}
 export const MarkdownContext = createContext<any>({});
 export function MarkdownViewer({ contents, meta }: ViewerProps) {
   const [markdown, setMarkdown] = useState(contents);
@@ -54,9 +45,15 @@ export function MarkdownViewer({ contents, meta }: ViewerProps) {
       per_page: 100,
       sort: 'updated',
     });
-    console.log(issues.data);
+    const releases = await octokit.repos.listReleases({
+      owner: meta.owner,
+      repo: meta.repo,
+      per_page: 100,
+    });
+    console.log(releases)
     const info = {
-      issues: issues.data
+      issues: issues.data,
+      releases: releases.data,
     }
     setRepoInfo(info);
   }
@@ -104,6 +101,7 @@ export function MarkdownViewer({ contents, meta }: ViewerProps) {
 }
 const components = {
   Issues,
+  Releases,
   code({ node, inline, className, children }) {
     const match = /language-(\w+)/.exec(className || '')
     return !inline && match ? (
@@ -122,6 +120,7 @@ const components = {
   }
 }
 
+const formatDate = timeFormat("%B %-d");
 const issueStateToStatusMap = {
   closed: "issueClosed",
   open: "issueOpened",
@@ -133,14 +132,43 @@ function Issues({ num = 3, children }: {
   const { issues } = useContext(MarkdownContext);
   const filteredIssues = issues.slice(0, num);
   return (
-    <div className="mb-6 ">
+    <div className="mt-3 mb-6">
       <div className="flex space-x-2 overflow-x-auto">
         {filteredIssues.map(issue => (
           <Box bg="canvas.subtle" p={3} key={issue.id} className="relative flex-1">
-            <StateLabel status={issueStateToStatusMap[issue.state]} variant="small" className="mb-1">
-              {issue.state}
-            </StateLabel>
-            <a className="block text-black" href={issue.html_url}>{issue.title}</a>
+            <div className="flex justify-between items-center">
+              <a className="block text-black text-lg" href={issue.html_url}>{issue.title}</a>
+              <StateLabel status={issueStateToStatusMap[issue.state]} variant="small" className="">
+                {issue.state}
+              </StateLabel>
+            </div>
+            <div className="mt-1 text-sm italic text-gray-600">
+              Last update {formatDate(new Date(issue.updated_at))}
+            </div>
+            {/* <MDX className="whitespace-pre-wrap truncate">{issue.body}</MDX> */}
+          </Box>
+        ))}
+      </div>
+    </div>
+  )
+}
+function Releases({ num = 3, children }: {
+  num: number,
+  children?: React.ReactNode
+}) {
+  const { releases = [] } = useContext(MarkdownContext);
+  const filteredReleases = releases.slice(0, num);
+  return (
+    <div className="mt-3 mb-6">
+      <div className="flex space-x-2 overflow-x-auto">
+        {filteredReleases.map(release => (
+          <Box bg="canvas.subtle" p={3} key={release.id} className="relative flex-1">
+            <div className="flex justify-between items-center">
+              <a className="block text-black text-lg" href={release.html_url}>{release.tag_name}</a>
+            </div>
+            <div className="mt-1 text-sm italic text-gray-600">
+              {formatDate(new Date(release.published_at))}
+            </div>
             {/* <MDX className="whitespace-pre-wrap truncate">{issue.body}</MDX> */}
           </Box>
         ))}

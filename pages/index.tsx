2d74ae1b9279741f86ed6b8cec2bdd5370e428b6
Irene Alvarado <irealva@github.com>
2021-11-16 10:09:48 -0500

    default to readme path in test folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         </div>
         <div className="mt-4">
           {session ? (
-            <Link href="/githubnext/composable-github-example-files">
+            <Link href="/githubnext/composable-github-example-files?path=README.md">
               <a className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                 View Sample Repository
               </a>

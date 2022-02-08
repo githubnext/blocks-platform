41b88d0dbb77158acc183efa8697bf951c2244dc
Amelia Wattenberger <wattenberger@github.com>
2022-02-08 10:35:43 -0500

    security review: add security headers

--- next.config.js
+++ next.config.js
@@ -7,5 +7,26 @@ module.exports = {
   },
   env: {
     PASSWORD_PROTECT: process.env.NEXT_PUBLIC_VERCEL_ENV !== 'development',
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [{
+          key: "Strict-Transport-Security",
+          value: "max-age=63072000; includeSubDomains; preload",
+        }, {
+          //   key: "X-Frame-Options",
+          //   value: "SAMEORIGIN",
+          // }, {
+          key: "X-XSS-Protection",
+          value: "0",
+        }, {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        ],
+      },
+    ]
+  },
 }

f28e9380f4f8220813d86af95f49ace5eadb6fa6
Matt Rothenberg <mattrothenberg@github.com>
2022-02-15 15:12:09 -0500

    fix: move site password to env var

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,6 @@
 import { loginHandler } from "@storyofams/next-password-protect";
 
-export default loginHandler("espresso", {
+export default loginHandler(process.env.NEXT_BLOCKS_PASSWORD, {
   cookieName: "blocks-password-protect",
   cookieSameSite: "Lax",
 });
--- pages/api/passwordCheck.js
+++ pages/api/passwordCheck.js
@@ -1,6 +1,6 @@
 import { passwordCheckHandler } from "@storyofams/next-password-protect";
 
-export default passwordCheckHandler("espresso", {
+export default passwordCheckHandler(process.env.NEXT_BLOCKS_PASSWORD, {
   cookieName: "blocks-password-protect",
   cookieSameSite: "Lax",
 });

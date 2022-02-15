34872d6da99e9e4989146cb39d2e34b05960ce21
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2022-02-15 15:09:34 -0500

    fix: ensure pw cookie has lax samesite policy (#27)

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,4 +2,5 @@ import { loginHandler } from "@storyofams/next-password-protect";
 
 export default loginHandler("espresso", {
   cookieName: "blocks-password-protect",
+  cookieSameSite: "Lax",
 });

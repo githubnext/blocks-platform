05488d72c877ed99aa6feb04ade5c49adb58ff4a
Matt Rothenberg <mattrothenberg@github.com>
2022-02-22 15:02:07 -0500

    remove unauthed state from detail page

--- pages/[owner]/[repo]/index.tsx
+++ pages/[owner]/[repo]/index.tsx
@@ -1,38 +1,18 @@
 import { withPasswordProtect } from "@storyofams/next-password-protect";
 import { FullPageLoader } from "components/full-page-loader";
 import { RepoDetail } from "components/repo-detail";
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
-const unauthenticatedRepos = [
-  "githubnext/blocks-tutorial"
-]
 function RepoDetailContainer() {
-  const router = useRouter();
-  const { repo, owner } = router.query;
-  const isViewingUnauthenticatedRepo = unauthenticatedRepos.includes(`${owner}/${repo}`);
-
   const { data: session, status } = useSession({
-    required: isViewingUnauthenticatedRepo,
-    onUnauthenticated: () => {
-      if (isViewingUnauthenticatedRepo) return
-      // @ts-ignore
-      signIn("github");
-    },
+    required: true,
   });
 
-  useEffect(() => {
-    if (!isViewingUnauthenticatedRepo && status === "unauthenticated") {
-      router.push("/");
-    }
-  }, [status, isViewingUnauthenticatedRepo])
-
-  if (!isViewingUnauthenticatedRepo && status === "loading") {
+  if (status === "loading") {
     return <FullPageLoader />;
   }
 
-  if (isViewingUnauthenticatedRepo || (status === "authenticated" && session)) {
+  if (status === "authenticated" && session) {
     // @ts-ignore
     return <RepoDetail token={session?.token} />;
   }
@@ -43,4 +23,4 @@ function RepoDetailContainer() {
 
 export default process.env.PASSWORD_PROTECT
   ? withPasswordProtect(RepoDetailContainer)
-  : RepoDetailContainer
+  : RepoDetailContainer;

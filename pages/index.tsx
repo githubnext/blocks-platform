09cf472bdbb9856dc968c5c248f5376fdc3b8d8b
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2022-02-22 15:09:51 -0500

    feat: auth wall for index page (#36)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { withPasswordProtect } from "@storyofams/next-password-protect";
 import { AnimatedBlocks } from "components/AnimatedBlocks";
 import { FullPageLoader } from "components/full-page-loader";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 function Home() {
-  const { data: session, status } = useSession({ required: false });
+  const { status } = useSession({ required: true });
 
   if (status === "loading") {
     return <FullPageLoader />;
@@ -16,24 +16,13 @@ function Home() {
       <div className="w-full px-4 lg:px-0 flex flex-col items-center justify-center z-10">
         <h1 className="text-[10vw] font-bold tracking-tighter text-gray-800 leading-[0.8em]">
           GitHub
-          <span className="font-light ml-5">
-            Blocks
-          </span>
+          <span className="font-light ml-5">Blocks</span>
         </h1>
         <p className="text-2xl tracking-[0.01em] font-light text-gray-500 leading-8 mt-7 max-w-2xl text-center mx-auto">
-          What GitHub would look and feel like if end
-          users could control how they interact with their content?
+          What GitHub would look and feel like if end users could control how
+          they interact with their content?
         </p>
         <div className="mt-10 space-x-4">
-          {!session && (
-            <button
-              // @ts-ignore
-              onClick={() => signIn("github")}
-              className="inline-flex items-center px-8 py-4 text-lg border border-transparent leading-4 font-medium rounded-md shadow-sm text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              Sign in with GitHub
-            </button>
-          )}
           <Link href="/githubnext/blocks-tutorial?path=README.md">
             <a className="inline-flex items-center px-8 py-4 text-lg border border-transparent leading-4 font-medium rounded-md shadow-sm text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
               View Sample Repository
@@ -41,17 +30,16 @@ function Home() {
           </Link>
         </div>
         <div className="mt-6 text-2xl flex items-center">
-          <div className="font-light text-xl">
-            an exploration by
-          </div>
-          <img className="w-[1.5em] ml-2" src="/next-octocat.svg" alt="GitHub Next logo" />
+          <div className="font-light text-xl">an exploration by</div>
+          <img
+            className="w-[1.5em] ml-2"
+            src="/next-octocat.svg"
+            alt="GitHub Next logo"
+          />
           <div className="font-bold tracking-tight">
             GitHub
-            <span className="font-normal ml-1">
-              Next
-            </span>
+            <span className="font-normal ml-1">Next</span>
           </div>
-
         </div>
       </div>
       <div className="absolute inset-0 z-0">
@@ -61,6 +49,4 @@ function Home() {
   );
 }
 
-export default process.env.PASSWORD_PROTECT
-  ? withPasswordProtect(Home)
-  : Home
+export default process.env.PASSWORD_PROTECT ? withPasswordProtect(Home) : Home;

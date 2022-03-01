354b3f0e11e8f1d45585c1aeb63e7eef14b871b4
Matt Rothenberg <mattrothenberg@github.com>
2022-03-01 17:53:07 -0500

    fix: remove unused variable

--- pages/[owner]/[repo]/index.tsx
+++ pages/[owner]/[repo]/index.tsx
@@ -1,10 +1,8 @@
 import { FullPageLoader } from "components/full-page-loader";
 import { RepoDetail } from "components/repo-detail";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 
 function RepoDetailContainer() {
-  const router = useRouter();
   const { data: session, status } = useSession({
     required: true,
   });

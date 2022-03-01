250e489657328757ba124e7683e09ce6295c18bb
Matt Rothenberg <mattrothenberg@github.com>
2022-03-01 17:30:27 -0500

    feat: track page views

---
+++ lib/analytics.ts
@@ -0,0 +1,34 @@
+interface TrackParams {
+  url: string;
+  event: string;
+  payload?: object;
+}
+
+export function track(params: TrackParams) {
+  const { url, event, payload } = params;
+  if (typeof window == "undefined") return;
+
+  if (process.env.NODE_ENV === "development") {
+    console.info("Analytics Disabled in development", params);
+    return;
+  }
+
+  return fetch({
+    url: "https://octo-metrics.azurewebsites.net/api/CaptureEvent",
+    method: "POST",
+    // @ts-ignore
+    body: JSON.stringify({
+      container: "blocks.githubnext.com",
+      event,
+      payload: {
+        url,
+        referrer: document.referrer,
+        source: "blocks.githubnext.com",
+        dimensions: [screen.width, screen.height].join(","),
+        userAgent: navigator.userAgent,
+        language: navigator.language,
+        ...payload,
+      },
+    }),
+  });
+}
--- pages/[owner]/[repo]/index.tsx
+++ pages/[owner]/[repo]/index.tsx
@@ -1,8 +1,10 @@
 import { FullPageLoader } from "components/full-page-loader";
 import { RepoDetail } from "components/repo-detail";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function RepoDetailContainer() {
+  const router = useRouter();
   const { data: session, status } = useSession({
     required: true,
   });

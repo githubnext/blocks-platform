e3466a5cd88d0d760b912b66ac6ff3846fe0c554
Amelia Wattenberger <wattenberger@github.com>
2022-04-20 16:49:59 -0700

    specify microsoft org for openai endpoints

--- pages/api/openai-edit.ts
+++ pages/api/openai-edit.ts
@@ -4,6 +4,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
+  organization: "org-rocrupyvzgcl4yf25rqq6d1v",
 });
 
 const openai = new OpenAIApi(configuration);

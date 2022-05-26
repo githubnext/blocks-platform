f4745b2d65f4a6e0daad9db5687190a6ec1cf8b3
Jake Donham <jaked@github.com>
2022-05-26 15:40:18 -0700

    less log

--- components/block-picker.tsx
+++ components/block-picker.tsx
@@ -62,7 +62,6 @@ export default function BlockPicker(props: BlockPickerProps) {
       enabled: isSearchTermUrl,
     }
   );
-  console.log(blocksUrlBlocks);
 
   const blocksList = isSearchTermUrl
     ? [blocksUrlBlocks].filter(Boolean)

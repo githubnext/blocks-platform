035746bfd678e234ef0e80466a35c940c65ee511
Jake Donham <jaked@github.com>
2022-09-30 13:30:43 -0700

    send block view telemetry to Azure App Insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,59 @@
 import "styles/index.css";
 import "styles/markdown.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider, BaseStyles } from "@primer/react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { track } from "../lib/analytics";
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
   const [queryClient] = useState(() => new QueryClient());
-
+  const appInsightsRef = useRef();
   const router = useRouter();
 
   useEffect(() => {
     if (process.env.NODE_ENV !== "production") return;
 
-    const appInsights = new ApplicationInsights({
+    appInsightsRef.current = new ApplicationInsights({
       config: {
         // this isn't a high-security key, see https://stackoverflow.com/questions/54535275/what-will-happen-if-applicationinsights-instrumentationkey-gets-stolen
         connectionString:
           "InstrumentationKey=96006ad9-5042-466e-b5c9-641be3a9e13f;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/",
       },
     });
-    appInsights.loadAppInsights();
+    appInsightsRef.current.loadAppInsights();
   }, []);
 
   useEffect(() => {
     const handleRouteChange = (url, { shallow }) => {
-      if (shallow) return;
-      let queryParams = url.split("?")[1];
-      const parsed = new URLSearchParams(queryParams);
-      const blockKey = parsed.get("blockKey");
-      const path = parsed.get("path");
-      if (path && blockKey) {
-        track({
-          url,
-          event: "block-view",
-          payload: {
-            blockKey,
-            path,
-          },
-        });
-      }
+      if (!appInsightsRef.current) return;
+
+      const parsedUrl = new URL(url, window.location.origin);
+      const params = parsedUrl.searchParams;
+      const pathname = parsedUrl.pathname;
+      const [_, owner, repo] = pathname.split("/");
+      const path = params.get("path");
+      const branch = params.get("branch");
+      const [blockOwner, blockRepo, blockId] = params
+        .get("blockKey")
+        .split("__");
+      const fileRef = params.get("fileRef");
+
+      const event = {
+        name: "block-view",
+        owner,
+        repo,
+        branch,
+        path,
+        fileRef,
+        blockOwner,
+        blockRepo,
+        blockId,
+        shallow,
+      };
+      appInsightsRef.current.trackEvent(event);
     };
 
     router.events.on("routeChangeComplete", handleRouteChange);

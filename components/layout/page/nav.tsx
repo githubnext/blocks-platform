83f48e47d1953aba757cf59bb1ba4c0346fc12fa
Jake Donham <jaked@github.com>
2022-11-08 10:35:59 -0800

    fix nav on signup pages

--- components/layout/page/nav.tsx
+++ components/layout/page/nav.tsx
@@ -6,24 +6,14 @@ export function Nav() {
       <div className="h-1 bg-gradient-to-r from-[#9867f0] to-[#ed4e50]"></div>
       <header className="p-4">
         <div className="flex items-center justify-between">
-          <Link href="/">
-            <a aria-label="Home" className="inline-block rounded-full">
-              <img
-                alt="GitHub Next Logo"
-                width={72}
-                height={72}
-                src="/assets/images/next-octocat.svg"
-              />
-            </a>
-          </Link>
-          <nav className="space-x-4 text-sm text-gray-600">
-            <a
-              className="hover:underline"
-              href="https://twitter.com/githubnext"
-            >
-              Twitter
-            </a>
-          </nav>
+          <div aria-label="Home" className="inline-block rounded-full">
+            <img
+              alt="GitHub Next Logo"
+              width={72}
+              height={72}
+              src="/assets/images/next-octocat.svg"
+            />
+          </div>
         </div>
       </header>
     </div>

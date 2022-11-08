f037cb3fcf54490aa6fcf8d0c46cabd3f1746511
Amelia Wattenberger <wattenberger@github.com>
2022-11-08 09:57:49 -0800

    landing page updates

--- components/gradient-badge.tsx
+++ components/gradient-badge.tsx
@@ -1,7 +1,7 @@
 export function GradientBadge({ children }: { children: React.ReactNode }) {
   return (
-    <div className="py-2 z-1 px-6  relative bg-transparent inline-flex items-center justify-center  rounded-full from-[#9867f0] to-[#ed4e50]">
-      <span className="z-10 text-sm lg:text-base bg-clip-text text-white font-semibold relative -top-px">
+    <div className="py-2 z-1 px-6 gradient-border-badge relative bg-transparent inline-flex items-center justify-center">
+      <span className="z-10 text-sm lg:text-base bg-clip-text text-transparent bg-gradient-to-r from-[#9867f0] to-[#ed4e50] font-semibold relative -top-px">
         {children}
       </span>
     </div>
@@ -16,9 +16,9 @@ export function GradientBadgeDark({
 }) {
   return (
     <div
-      className={`${className} pt-[0.3em] pb-[0.4em] z-1 px-[1em]  rounded-full bg-gradient-to-r from-[#79C0FF] to-[#84EDC1] relative bg-transparent inline-flex items-center justify-center`}
+      className={`${className} pt-[0.3em] pb-[0.4em] z-1 px-[1em] gradient-border-badge-dark relative bg-transparent inline-flex items-center justify-center`}
     >
-      <span className="z-10 text-sm lg:text-base bg-clip-text text-gh-marketingDark font-normal relative -top-px leading-none">
+      <span className="z-10 text-sm lg:text-base bg-clip-text text-transparent bg-gradient-to-r from-[#79C0FF] to-[#84EDC1] font-normal relative -top-px leading-none">
         {children}
       </span>
     </div>
